afee8110f3f1e8c7f438c7d33e5aa630b1807991
sam <aspirantbelosheev@tut.by>
2024-04-14 23:48:25 +0300

    55555

--- src/app/prew/prew.component.ts
+++ src/app/prew/prew.component.ts
@@ -6,5 +6,51 @@ import { Component } from '@angular/core';
   styleUrl: './prew.component.css'
 })
 export class PrewComponent {
-
+  more1 = false
+  more2 = false
+  more3 = false
+  more4 = false
+  more5 = false
+  more6 = false
+  more7 = false
+  more8 = false
+  more9 = false
+  more10 = false
+  more11 = false
+  more12 = false
+  more13 = false
+  more14 = false
+  more15 = false
+  more16 = false
+  more17 = false
+  more18 = false
+  more19 = false
+  more20 = false
+  more21 = false
+  more22 = false
+  more23 = false
+  more25 = false
+  more24 = false
+  more26 = false
+  more27 = false
+  more28 = false
+  more29 = false
+  more30 = false
+  more31 = false
+  more32 = false
+  more33 = false
+  more34 = false
+  more35 = false
+  more36 = false
+  more37 = false
+  more38 = false
+  more39 = false
+  more40 = false
+  more41 = false
+  more42 = false
+  more43 = false
+  more44 = false
+  more45 = false
+  more46 = false
+  more47 = false
 }
