593d3cc21e0d546aefe4414f4cf8c98f1da16967
sam <aspirantbelosheev@tut.by>
2024-06-25 21:43:42 +0300

    products update

--- src/app/Company/strategy.component.ts
+++ src/app/Company/strategy.component.ts
@@ -19,6 +19,30 @@ export class StrategyComponent {
   show5 = false
   show6 = false
   show7 = false
+  more = false
+  more1 = false
+  more2 = false
+  more3 = false
+  more4 = false
+  more5 = false
+  more6 = false
+  more7 = false
+  more8 = false
+  more9 = false
+  more10 = false
+  more11 = false
+  more12 = false
+  more13 = false
+  more14 = false
+
+
+  under = false
+  under1 = false
+  under2 = false
+  under3 = false
+  under4 = false
+  under5 = false
+
 }
 
 
