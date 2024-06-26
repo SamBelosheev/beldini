76c7ebc06c772b714985e93ebc9aa49c16719803
belosheev_e <belosheev_e@lwo.by>
2024-06-26 11:22:04 +0300

    fix tabs

--- src/app/prew/prew.component.ts
+++ src/app/prew/prew.component.ts
@@ -6,54 +6,39 @@ import { Component } from '@angular/core';
   styleUrl: './prew.component.css'
 })
 export class PrewComponent {
-  more1 = false
-  more2 = false
-  more3 = false
-  more4 = false
-  more5 = false
-  more6 = false
-  more7 = false
-  more8 = false
-  more9 = false
-  more10 = false
-  more11 = false
-  more12 = false
-  more13 = false
-  more14 = false
-  more15 = false
-  more16 = false
-  more17 = false
-  more18 = false
-  more19 = false
-  more20 = false
-  more21 = false
-  more22 = false
-  more23 = false
-  more25 = false
-  more24 = false
-  more26 = false
-  more27 = false
-  more28 = false
-  more29 = false
-  more30 = false
-  more31 = false
-  more32 = false
-  more33 = false
-  more34 = false
-  more35 = false
-  more36 = false
-  more37 = false
-  more38 = false
-  more39 = false
-  more40 = false
-  more41 = false
-  more42 = false
-  more43 = false
-  more44 = false
-  more45 = false
-  more46 = false
-  more47 = false
+  currentTab: number;
+  tabs: {id: number; name: string}[] =
+      [
+        {id: 1, name: 'История бренда'},
+        {id: 2, name: 'Товарный знак'},
+        {id: 3, name: 'Команда'},
+        {id: 4, name: 'Миссия'},
+        {id: 5, name: 'Фабрика мрамора'},
+        {id: 6, name: 'Галерея мрамора'},
+        {id: 7, name: 'Продукция'},
+        {id: 8, name: 'Сырьё для мраморной фабрики'},
+        {id: 9, name: 'Стоимость изделий'},
+        {id: 10, name: 'Авторские права'},
+        {id: 11, name: 'Сертификаты качества'},
+        {id: 12, name: 'Патенты'},
+        {id: 13, name: 'Договор на приобретение'},
+        {id: 14, name: 'Гарантийные обязательства'},
+        {id: 15, name: 'Выгода приобретения'},
+        {id: 16, name: 'Услуги'},
+        {id: 17, name: 'Проектирование и модернизация'},
+        {id: 18, name: 'Подбор камня'},
+        {id: 19, name: 'Изготовление изделий'},
+        {id: 20, name: 'Логистика'},
+        {id: 21, name: 'Монтаж'},
+        {id: 22, name: 'Сотрудничество'},
+        {id: 23, name: 'Франшиза'},
+        {id: 24, name: 'Инвестиции в проект'},
+        {id: 25, name: 'Экскурсия на завод'},
+        {id: 26, name: 'Благотворительность'},
+        {id: 27, name: 'Пожертвование в проект'}
+      ]
 
-
-
+  setTab(tab: number) {
+    this.currentTab = tab;
+  }
 }
